fd7b6bf71bdaeb0294916813b96852db114d87ba
Ilan Yashuk <ilan147963@gmail.com>
2025-08-23 13:18:07 +0300

    Added centering and underline outside control

--- src/components/text/HeaderText.tsx
+++ src/components/text/HeaderText.tsx
@@ -1,12 +1,24 @@
 interface HeaderProps {
   children: React.ReactNode;
+  disableunderline?: boolean;
+  centertext?: boolean;
 }
 
-const HeaderText: React.FC<HeaderProps> = ({ children }) => {
+const HeaderText: React.FC<HeaderProps> = ({
+  children,
+  disableunderline,
+  centertext,
+}) => {
   return (
-    <h1 className="text-5xl border-b-4 border-[var(--third)] font-bold w-fit mb-2 font-dm-serif">
-      {children}
-    </h1>
+    <div className={`flex ${centertext ? "justify-center" : ""}`}>
+      <h1
+        className={`text-5xl border-[var(--third)] font-bold w-fit mb-2 font-dm-serif ${
+          disableunderline ? "border-b-0" : "border-b-4"
+        }`}
+      >
+        {children}
+      </h1>
+    </div>
   );
 };
 
